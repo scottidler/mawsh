8d38e6de284ce3c855c7228b1fe9901b5a120ced
Scott A. Idler <scott.a.idler@gmail.com>
2020-04-25 14:13:11 -0700

    keeping

--- shell-functions.sh
+++ shell-functions.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
+if [ -n "$DEBUG" ]; then
+    PS4=':${LINENO}+'
+    set -x
+fi
+
+REAL_FILE="$0"
+REAL_NAME="$(basename "$REAL_FILE")"
+REAL_PATH="$(dirname "$REAL_FILE")"
+if [ -L "$0" ]; then
+    LINK_FILE=$REAL_FILE; REAL_FILE="$(readlink "$0")"
+    LINK_NAME=$REAL_NAME; REAL_NAME="$(basename "$REAL_FILE")"
+    LINK_PATH=$REAL_PATH; REAL_PATH="$(dirname "$REAL_FILE")"
+fi
+
+. $REAL_PATH/bin/mawsh
 function mawsh() {
-    exec bin/mawsh
+    _mawsh_main "$@"
 }
 
+#function testy() {
+#    . $REAL_PATH/bin/testy
+#    do_testy
+#}
